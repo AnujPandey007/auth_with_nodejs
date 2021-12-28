120dd34c58a3706734e4a3c342a2c394b7e21a1e
anuj <panuj330@gmail.com>
2021-12-28 11:27:01 +0530

    issue 3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Imports
 const express = require('express');
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 // const scriptFile = require("../First Web App/public/js/script.js");
 // console.log(scriptFile.adminName);
